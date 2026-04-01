78267f6274cc11b084f533d69a4bd5c9aab1acac
parulljain2003 <paruljain@goldgini.com>
2026-04-02 01:06:23 +0530

    removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import HeroBanner from './components/HeroBanner/HeroBanner';
-import CategoryCards from './components/CategoryCards/CategoryCards';
-import CategoryGrid from './components/CategoryGrid/CategoryGrid';
 import Products from './components/Products/Products';
 
 export default function Home() {
   return (
     <main style={{ backgroundColor: '#fff', minHeight: '100vh' }}>
       <HeroBanner />
-      <CategoryCards />
-      <CategoryGrid />
       <Products />
     </main>
   );
